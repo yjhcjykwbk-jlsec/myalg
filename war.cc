023ea9571044ef2efb064daf6299930f4edb7f37
xzg <x@ubuntu.(none)>
2013-03-24 00:49:33 +0800

    ...

--- war.cc
+++ war.cc
@@ -26,34 +26,30 @@ class Comander{
 			for(int i=0;i<5;i++) craftNum[i]=0;
 		}
 		void addCraft(Craft* pCraft);
-		Craft* makeCraft();
+		Craft* makeCraft(int time);
 		int getCraftNum(int i){return craftNum[i];}
 };
 int Comander::craftLife0[5]={0,1,2,3,4};
 class Craft{
 	protected:
+		int birthtime;
 		int number;
 		Comander *cmd;//阵营
 		int life;//生命值
 	public:
-		static const int craftType=-1;
-		Craft(Comander *comander,int num,int life):cmd(comander),number(num),life(life){
+		Craft(Comander *comander,int num,int life,int time):cmd(comander),number(num),life(life),birthtime(time){
 			comander->lifeEle-=this->life;//每生成一个Craft，将消耗总部生命元
 		}
-		Craft():cmd(NULL),life(0),number(-1){}
-		virtual int getCraftType(){return craftType;}	
+		virtual int getCraftType(){return -1;}	
 		const char *getCraftName(){
 			switch(getCraftType()){
-				case(0): return "dragon";break;
+				case(0):return "dragon";break;
 				case(1):return "ninja";break;
 				case(2):return "iceman";break;
 				case(3):return "lion";break;
 				case(4):return "wolf";break;
 				default:return "";
 			}
-		}
-		virtual void introduce(){
-			cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
 		}
 };
 class Arm{
@@ -91,67 +87,76 @@ class Dragon:public Craft{
 		Arm* pArm;
 		float morale;//士气
 	public:
-		static const int craftType=0;
-		Dragon(Comander* cmd,int num,int life):Craft(cmd,num,life){
+		Dragon(Comander* cmd,int num,int life,int time):Craft(cmd,num,life,time){
 			morale=(float)cmd->lifeEle/life;
 			pArm=ArmFactory::getArm(num%3);
+			cmd->addCraft(this);
+			cout.width(3);
+			cout.fill('0');
+			cout<<birthtime<<" ";
+			cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
+			cout<<"It has a "<<pArm->getName()<<",and it's morale is "; cout.precision(3);cout<<morale<<endl;
 		}
-		int getCraftType(){return craftType;}	
-		void introduce(){
-			Craft::introduce();
-			cout<<"It has a "<<pArm->getName()<<",and it's morale is ";
-			cout.precision(3);cout<<morale<<endl;
-		}
+		int getCraftType(){return 0;}	
 };
 class Ninja :public Craft{
 	Arm *pArm1,*pArm2;
 	public:
-	static const int craftType=1;
-	Ninja(Comander* cmd,int num,int life):Craft(cmd,num,life){
+	Ninja(Comander* cmd,int num,int life,int time):Craft(cmd,num,life,time){
 		pArm1=ArmFactory::getArm(num%3);
 		pArm2=ArmFactory::getArm((1+num)%3);
+			cmd->addCraft(this);
+		cout.width(3);
+		cout.fill('0');
+		cout<<birthtime<<" ";
+		cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
+		cout<<"It has a "<<pArm1->getName()<<" and a "<<pArm2->getName()<<endl; 
 	}
-	int getCraftType(){return craftType;}	
-	void introduce(){ 
-		Craft::introduce();
-		cout<<"It has a "<<pArm1->getName()<<" and a "<<pArm2->getName()<<endl; }
+	int getCraftType(){return 1;}	
 };
 
 class Iceman :public Craft{ 
 	private:
 		Arm* pArm;
 	public:
-		static const int craftType=2;
-		Iceman(Comander* cmd,int num,int life):Craft(cmd,num,life){
+		Iceman(Comander* cmd,int num,int life,int time):Craft(cmd,num,life,time){
 			pArm=ArmFactory::getArm(num%3);
-		}
-		int getCraftType(){return craftType;}	
-		void introduce(){ 
-			Craft::introduce();
+			cmd->addCraft(this);
+			cout.width(3);
+			cout.fill('0');
+			cout<<birthtime<<" ";
+			cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
 			cout<<"It has a "<<pArm->getName()<<endl;}
+		int getCraftType(){return 2;}	
 };
 class Lion :public Craft{ 
 	private:
 		int loyalty;//忠诚度
 	public:
 		static const int craftType=3;
-		Lion(Comander* cmd,int num,int life):Craft(cmd,num,life){
+		Lion(Comander* cmd,int num,int life,int time):Craft(cmd,num,life,time){
 			loyalty=cmd->lifeEle;//生命元数量
-		}
-		int getCraftType(){return craftType;}	
-		void introduce(){ 
-			Craft::introduce();
+			cmd->addCraft(this);
+			cout.width(3);
+			cout.fill('0');
+			cout<<birthtime<<" ";
+			cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
 			cout<<"It's loyalty is "<<loyalty<<endl;
 		}
+		int getCraftType(){return 3;}	
 };
 class Wolf :public Craft{
 	public:
-		static const int craftType=4;
-		int getCraftType(){return craftType;}	
-		Wolf(Comander* cmd,int num,int life):Craft(cmd,num,life){}
-			void introduce(){ Craft::introduce(); }
+		Wolf(Comander* cmd,int num,int life,int time):Craft(cmd,num,life,time){
+			cmd->addCraft(this);
+			cout.width(3);
+			cout.fill('0');
+			cout<<birthtime<<" ";
+			cout<<cmd->getColor()<<" "<<getCraftName()<<" "<<number<<" born with strength "<<life<<","<<cmd->getCraftNum(getCraftType())<<" "<<getCraftName()<<" in "<<cmd->getColor()<<" headquarter"<<endl;
+		}
+		int getCraftType(){return 4;}	
 };
-Craft *Comander::makeCraft(){
+Craft *Comander::makeCraft(int time){
 	if(lifeEle<=0) return NULL;
 	Craft *craft=NULL;
 	if(color==Red){
@@ -159,23 +164,23 @@ Craft *Comander::makeCraft(){
 			switch(next){
 				case(0): 
 					if(lifeEle>=getIcemanLife())
-					craft=new Iceman(this,++startNum,getIcemanLife());
+					craft=new Iceman(this,++startNum,getIcemanLife(),time);
 					break;
 				case(1):
 					if(lifeEle>=getLionLife())
-					craft=new Lion(this,++startNum,getLionLife());
+					craft=new Lion(this,++startNum,getLionLife(),time);
 					break;
 				case(2):
 					if(lifeEle>=getWolfLife())
-					craft=new Wolf(this,++startNum,getWolfLife());
+					craft=new Wolf(this,++startNum,getWolfLife(),time);
 					break;
 				case(3): 
 					if(lifeEle>=getNinjaLife())
-					craft=new Ninja(this,++startNum,getNinjaLife());
+					craft=new Ninja(this,++startNum,getNinjaLife(),time);
 					break;
 				case(4): 
 					if(lifeEle>=getDragonLife())
-					craft=new Dragon(this,++startNum,getDragonLife());
+					craft=new Dragon(this,++startNum,getDragonLife(),time);
 					break;
 			}
 		}
@@ -185,27 +190,26 @@ Craft *Comander::makeCraft(){
 			switch(next){
 				case(0): 
 					if(lifeEle>=getLionLife())
-					craft=new Lion(this,++startNum,getLionLife());
+					craft=new Lion(this,++startNum,getLionLife(),time);
 					break;
 				case(1): 
 					if(lifeEle>=getDragonLife())
-					craft=new Dragon(this,++startNum,getDragonLife());
+					craft=new Dragon(this,++startNum,getDragonLife(),time);
 					break;
 				case(2): 
 					if(lifeEle>=getNinjaLife())
-					craft=new Ninja(this,++startNum,getNinjaLife());
+					craft=new Ninja(this,++startNum,getNinjaLife(),time);
 					break;
 				case(3):
 					if(lifeEle>=getIcemanLife())
-					craft=new Iceman(this,++startNum,getIcemanLife());
+					craft=new Iceman(this,++startNum,getIcemanLife(),time);
 					break;
 				case(4):
 					if(lifeEle>=getWolfLife())
-					craft=new Wolf(this,++startNum,getWolfLife());
+					craft=new Wolf(this,++startNum,getWolfLife(),time);
 					break;
 			}
 		}
-	if(craft!=NULL) {addCraft(craft);}
 	return craft;
 }
 void Comander::addCraft(Craft* pCraft)
@@ -216,30 +220,30 @@ void Comander::addCraft(Craft* pCraft)
 int main(){
 	int m,n;
 	cin>>n;
-	while(n-->0){
-	cout<<"Case:"<<n+1<<endl;
-	cin>>m;//生命元
-	Comander *r=new Comander(Red,m);
-	Comander *b=new Comander(Blue,m);
-	for(int i=0;i<5;i++) cin>>Comander::craftLife0[i];
-	Craft *Craft1,*Craft2;
-	bool stopr=false,stopb=false;
-	for(int time=0;;time++){
-		if(!stopr) {
-			cout.width(3);
-			cout.fill('0');
-			Craft1=r->makeCraft();
-			if(Craft1!=NULL) {cout<<time<<" ";Craft1->introduce();}
-			else {stopr=true; cout<<time<<" red headquarter stops making warriors"<<endl;}
-		}
-		if(!stopb) {
-			cout.width(3);
-			cout.fill('0');
-			Craft2=b->makeCraft();
-			if(Craft2!=NULL){ cout<<time<<" ";Craft2->introduce();}
-			else {stopb=true; cout<<time<<" blue headquarter stops making warriors"<<endl;}
-		}
-		if(stopr&&stopb) break;
+	int data[1000][5];int mdata[1000];
+	for(int i=1;i<=n;i++){
+		cin>>mdata[i];
+		for(int j=0;j<5;j++) cin>>data[i][j];
+	}
+	int i=0;
+	while(++i<=n){
+		m=mdata[i];//生命元
+		Comander *r=new Comander(Red,m);
+		Comander *b=new Comander(Blue,m);
+		for(int j=0;j<5;j++) Comander::craftLife0[j]=data[i][j];
+		cout<<"Case:"<<i<<endl;
+		Craft *Craft1,*Craft2;
+		bool stopr=false,stopb=false;
+		for(int time=0;;time++){
+			if(!stopr) {
+				Craft1=r->makeCraft(time);
+				if(Craft1==NULL) {stopr=true; cout.width(3); cout.fill('0'); cout<<time<<" red headquarter stops making warriors"<<endl;}
+			}
+			if(!stopb) {
+				Craft2=b->makeCraft(time);
+				if(Craft2==NULL) {stopb=true; cout.width(3); cout.fill('0'); cout<<time<<" blue headquarter stops making warriors"<<endl;}
+			}
+			if(stopr&&stopb) break;
 		}
 	}
 	return 0;
