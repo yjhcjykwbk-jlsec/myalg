ab875359e12149705bbae27b55b3d34b3fd35bdc
stevenberge <x@ubuntu.(none)>
2013-03-25 14:37:39 +0800

    finish prison break?

--- prison.cc
+++ prison.cc
@@ -24,129 +24,135 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-  
+
 using namespace std;
 #define rep0(i,n) for(int i=0;i<n;i++)  //[0,n)
 #define rep(i,m,n) for(int i=m;i<n;i++) //[m,n)
 #define tep0(i,m) for(int i=m;i>0;i--)  //[m.0)
 #define tep(i,m,n) for(int i=m;i>n;i--) //[m.n)
 //BEGINTEMPLATE_BY_ACRUSH_TOPCODER
-#define SIZE(X) ((int)(X.size()))//NOTES:SIZE(
-#define LENGTH(X) ((int)(X.length()))//NOTES:LENGTH(
-#define MP(X,Y) make_pair(X,Y)//NOTES:MP(
-typedef long long int64;//NOTES:int64
-typedef unsigned long long uint64;//NOTES:uint64
-#define two(X) (1<<(X))//NOTES:two(
-#define twoL(X) (((int64)(1))<<(X))//NOTES:twoL(
-#define contain(S,X) (((S)&two(X))!=0)//NOTES:contain(
-#define containL(S,X) (((S)&twoL(X))!=0)//NOTES:containL(
-const double pi=acos(-1.0);//NOTES:pi
-const double eps=1e-11;//NOTES:eps
-template<class T> inline void checkmin(T &a,T b){if(b<a) a=b;}//NOTES:checkmin(
-template<class T> inline void checkmax(T &a,T b){if(b>a) a=b;}//NOTES:checkmax(
-template<class T> inline T sqr(T x){return x*x;}//NOTES:sqr
-typedef pair<int,int> ipair;//NOTES:ipair
-template<class T> inline T lowbit(T n){return (n^(n-1))&n;}//NOTES:lowbit(
-template<class T> inline int countbit(T n){return (n==0)?0:(1+countbit(n&(n-1)));}//NOTES:countbit(
-//Numberic Functions
-template<class T> inline T gcd(T a,T b)//NOTES:gcd(
-  {if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
-template<class T> inline T lcm(T a,T b)//NOTES:lcm(
-  {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
-template<class T> inline T euclide(T a,T b,T &x,T &y)//NOTES:euclide(
-  {if(a<0){T d=euclide(-a,b,x,y);x=-x;return d;}
-   if(b<0){T d=euclide(a,-b,x,y);y=-y;return d;}
-   if(b==0){x=1;y=0;return a;}else{T d=euclide(b,a%b,x,y);T t=x;x=y;y=t-(a/b)*y;return d;}}
-template<class T> inline vector<pair<T,int> > factorize(T n)//NOTES:factorize(
-  {vector<pair<T,int> > R;for (T i=2;n>1;){if (n%i==0){int C=0;for (;n%i==0;C++,n/=i);R.push_back(make_pair(i,C));}
-   i++;if (i>n/i) i=n;}if (n>1) R.push_back(make_pair(n,1));return R;}
-template<class T> inline bool isPrimeNumber(T n)//NOTES:isPrimeNumber(
-  {if(n<=1)return false;for (T i=2;i*i<=n;i++) if (n%i==0) return false;return true;}
-template<class T> inline T eularFunction(T n)//NOTES:eularFunction(
-  {vector<pair<T,int> > R=factorize(n);T r=n;for (int i=0;i<R.size();i++)r=r/R[i].first*(R[i].first-1);return r;}
-//Matrix Operations
-const int MaxMatrixSize=40;//NOTES:MaxMatrixSize
-template<class T> inline void showMatrix(int n,T A[MaxMatrixSize][MaxMatrixSize])//NOTES:showMatrix(
-  {for (int i=0;i<n;i++){for (int j=0;j<n;j++)cout<<A[i][j];cout<<endl;}}
-template<class T> inline T checkMod(T n,T m) {return (n%m+m)%m;}//NOTES:checkMod(
-template<class T> inline void identityMatrix(int n,T A[MaxMatrixSize][MaxMatrixSize])//NOTES:identityMatrix(
-  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) A[i][j]=(i==j)?1:0;}
-template<class T> inline void addMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:addMatrix(
-  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=A[i][j]+B[i][j];}
-template<class T> inline void subMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:subMatrix(
-  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=A[i][j]-B[i][j];}
-template<class T> inline void mulMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T _A[MaxMatrixSize][MaxMatrixSize],T _B[MaxMatrixSize][MaxMatrixSize])//NOTES:mulMatrix(
-  { T A[MaxMatrixSize][MaxMatrixSize],B[MaxMatrixSize][MaxMatrixSize];
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) A[i][j]=_A[i][j],B[i][j]=_B[i][j],C[i][j]=0;
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) for (int k=0;k<n;k++) C[i][j]+=A[i][k]*B[k][j];}
-template<class T> inline void addModMatrix(int n,T m,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:addModMatrix(
-  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=checkMod(A[i][j]+B[i][j],m);}
-template<class T> inline void subModMatrix(int n,T m,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:subModMatrix(
-  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=checkMod(A[i][j]-B[i][j],m);}
-template<class T> inline T multiplyMod(T a,T b,T m) {return (T)((((int64)(a)*(int64)(b)%(int64)(m))+(int64)(m))%(int64)(m));}//NOTES:multiplyMod(
-template<class T> inline void mulModMatrix(int n,T m,T C[MaxMatrixSize][MaxMatrixSize],T _A[MaxMatrixSize][MaxMatrixSize],T _B[MaxMatrixSize][MaxMatrixSize])//NOTES:mulModMatrix(
-  { T A[MaxMatrixSize][MaxMatrixSize],B[MaxMatrixSize][MaxMatrixSize];
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) A[i][j]=_A[i][j],B[i][j]=_B[i][j],C[i][j]=0;
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) for (int k=0;k<n;k++) C[i][j]=(C[i][j]+multiplyMod(A[i][k],B[k][j],m))%m;}
-template<class T> inline T powerMod(T p,int e,T m)//NOTES:powerMod(
-  {if(e==0)return 1%m;else if(e%2==0){T t=powerMod(p,e/2,m);return multiplyMod(t,t,m);}else return multiplyMod(powerMod(p,e-1,m),p,m);}
-//Point&Line
-double dist(double x1,double y1,double x2,double y2){return sqrt(sqr(x1-x2)+sqr(y1-y2));}//NOTES:dist(
-double distR(double x1,double y1,double x2,double y2){return sqr(x1-x2)+sqr(y1-y2);}//NOTES:distR(
-template<class T> T cross(T x0,T y0,T x1,T y1,T x2,T y2){return (x1-x0)*(y2-y0)-(x2-x0)*(y1-y0);}//NOTES:cross(
-int crossOper(double x0,double y0,double x1,double y1,double x2,double y2)//NOTES:crossOper(
-  {double t=(x1-x0)*(y2-y0)-(x2-x0)*(y1-y0);if (fabs(t)<=eps) return 0;return (t<0)?-1:1;}
-bool isIntersect(double x1,double y1,double x2,double y2,double x3,double y3,double x4,double y4)//NOTES:isIntersect(
-  {return crossOper(x1,y1,x2,y2,x3,y3)*crossOper(x1,y1,x2,y2,x4,y4)<0 && crossOper(x3,y3,x4,y4,x1,y1)*crossOper(x3,y3,x4,y4,x2,y2)<0;}
-bool isMiddle(double s,double m,double t){return fabs(s-m)<=eps || fabs(t-m)<=eps || (s<m)!=(t<m);}//NOTES:isMiddle(
-//Translator
-bool isUpperCase(char c){return c>='A' && c<='Z';}//NOTES:isUpperCase(
-bool isLowerCase(char c){return c>='a' && c<='z';}//NOTES:isLowerCase(
-bool isLetter(char c){return c>='A' && c<='Z' || c>='a' && c<='z';}//NOTES:isLetter(
-bool isDigit(char c){return c>='0' && c<='9';}//NOTES:isDigit(
-char toLowerCase(char c){return (isUpperCase(c))?(c+32):c;}//NOTES:toLowerCase(
-char toUpperCase(char c){return (isLowerCase(c))?(c-32):c;}//NOTES:toUpperCase(
-template<class T> string toString(T n){ostringstream ost;ost<<n;ost.flush();return ost.str();}//NOTES:toString(
-int toInt(string s){int r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt(
-int64 toInt64(string s){int64 r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt64(
-double toDouble(string s){double r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toDouble(
-template<class T> void stoa(string s,int &n,T A[]){n=0;istringstream sin(s);for(T v;sin>>v;A[n++]=v);}//NOTES:stoa(
-template<class T> void atos(int n,T A[],string &s){ostringstream sout;for(int i=0;i<n;i++){if(i>0)sout<<' ';sout<<A[i];}s=sout.str();}//NOTES:atos(
-template<class T> void atov(int n,T A[],vector<T> &vi){vi.clear();for (int i=0;i<n;i++) vi.push_back(A[i]);}//NOTES:atov(
-template<class T> void vtoa(vector<T> vi,int &n,T A[]){n=vi.size();for (int i=0;i<n;i++)A[i]=vi[i];}//NOTES:vtoa(
-template<class T> void stov(string s,vector<T> &vi){vi.clear();istringstream sin(s);for(T v;sin>>v;vi.push_bakc(v));}//NOTES:stov(
-template<class T> void vtos(vector<T> vi,string &s){ostringstream sout;for (int i=0;i<vi.size();i++){if(i>0)sout<<' ';sout<<vi[i];}s=sout.str();}//NOTES:vtos(
-//Fraction
-template<class T> struct Fraction{T a,b;Fraction(T a=0,T b=1);string toString();};//NOTES:Fraction
-  template<class T> Fraction<T>::Fraction(T a,T b){T d=gcd(a,b);a/=d;b/=d;if (b<0) a=-a,b=-b;this->a=a;this->b=b;}
-  template<class T> string Fraction<T>::toString(){ostringstream sout;sout<<a<<"/"<<b;return sout.str();}
-  template<class T> Fraction<T> operator+(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b+q.a*p.b,p.b*q.b);}
-  template<class T> Fraction<T> operator-(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b-q.a*p.b,p.b*q.b);}
-  template<class T> Fraction<T> operator*(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.a,p.b*q.b);}
-  template<class T> Fraction<T> operator/(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b,p.b*q.a);}
 //ENDTEMPLATE_BY_ACRUSH_TOPCODER
- 
+
+#define FAR 999999
+
+struct Pos{
+	int x;
+	int y;
+	Pos(int a,int b){x=a,y=b;}
+	Pos(){x=-1,y=-1;}
+};
 int n,m;
-int fn,fm;
-int maxn=20;
-int a[maxn][maxn];
-enum state{sense=0,empty=1,energy=2,power=3};
-int init(){
-	scanf("%d%d",&n,&m);
-	char t[20];
-	memset(a,0,sizeof(a)*sizeof(int));
-	rep(i,1,n){
-		scanf("%s",t);
-		rep(j,1,m) 
-		{
-			if(t[j]=='S'||t[j]=='F') a[i][j]=0;
-			else if(t[j]=='G') a[i][j]=1;
-			else if(t[j]=='Y') a[i][j]=2;
-			else if(t[j]=='D') a[i][j]=-1;
-			if(t[j]=='F') fn=i,fm=j;
+char a[20][20];
+int E;//最大电量
+int dst[16][16];//
+int number[16][16];
+bool isEPool[16];
+int fnum;//起始编号
+int tnum;//所有的个数
+Pos poses[16];//编号为i的位置
+int endstate;
+int direction[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+void initnum(){
+	rep0(i,n)rep0(j,m) number[i][j]=-1;//初始尚未编号
+	int num=0;
+	endstate=0;
+	rep0(i,16) isEPool[i]=false;
+	rep0(i,n)rep0(j,m) 
+		if(a[i][j]=='G'||a[i][j]=='Y'||a[i][j]=='F'){
+			number[i][j]=num,poses[num]=Pos(i,j);
+			if(a[i][j]=='Y') endstate+=(1<<(num));//必须关掉的灯
+			if(a[i][j]=='F') fnum=num;
+			if(a[i][j]=='G') isEPool[num]=true;//可以充电的地方
+			num++;
+		}
+	tnum=num;
+}
+int d[16][16];
+void getDst(int s){
+	rep0(i,n)rep0(j,m) d[i][j]=FAR;
+	queue<Pos> vqueue;
+	Pos p=poses[s];
+	vqueue.push(p);
+	d[p.x][p.y]=0;
+	while(!vqueue.empty()){
+		p=vqueue.front();vqueue.pop();
+		int x=p.x,y=p.y;
+		for(int i=0;i<4;i++){
+			int x1=x+direction[i][0],y1=y+direction[i][1];
+			if(a[x1][y1]!='D'&&d[x1][y1]==FAR) {
+				d[x1][y1]=d[x][y]+1;vqueue.push(Pos(x1,y1));
+			}
 		}
 	}
+	rep0(i,tnum){
+		p=poses[i];dst[s][i]=d[p.x][p.y];dst[i][s]=d[p.x][p.y];
+	}
 }
-int search
+void initdst(){
+	rep0(i,tnum) getDst(i);
+}
+bool init(){
+	scanf("%d%d",&n,&m);
+	if(n==0||m==0) return false;
+	rep0(i,n+2) rep0(j,m+2) a[i][j]='D';
+	char t[20];
+	rep(i,1,n+2)
+	{scanf("%s",t);
+		rep(j,0,m+2)
+			a[i][j+1]=t[j];
+	}
+//	rep0(i,n+1){rep0(j,m+1) cout<<a[i][j];cout<<endl;}
+	initnum();
+	initdst();
+	return true;
+}
+int energy[1<<16][16];
+void initenergy(){
+	//cout<<" tnum:"<<tnum<<endl;
+	rep0(i,1<<tnum)rep0(j,tnum) energy[i][j]=-1;
+}
+bool f(int state,int pos,int e){
+	if(state&endstate==endstate) return true;
+	//cout<<"i am at "<<pos<<" with energy "<<e<<" state "<<state<<endl;
+	rep0(i,tnum){
+		int d=dst[pos][i];
+		//cout<<"if go to "<<i<<" i need energy "<<d<<endl;
+		//cout<<"then state will change to "<<(state&(1<<i))<<endl;
+		//cout<<state<<":"<<(1<<i)<<endl;
+		if((state&(1<<i))==0&&d<=e)
+		{
+			//cout<<"i try go to "<<i<<" i need energy "<<d<<endl;
+			d=isEPool[i]?E:e-d;
+			if(energy[state|(1<<i)][i]<d){
+				if(f(state|(1<<i),i,d)) return true;
+			}
+		}
+	}
+	return false;
+}
+
+int search(){
+	int l=1,r=800;
+	while(true){
+		if(l>r) return -1;
+		int mid=(l+r)/2;
+		E=mid;
+		int state=1<<fnum;
+		initenergy();
+		energy[state][fnum]=E;//初始电量
+		if(f(state,fnum,E)) r=mid;
+		else l=mid+1;
+		if(l==r) return l;
+	}
+}
+
 int main(){
+		while(true){
+	if(!init()) return 0;
+	//cout<<"from F="<<fnum<<" to every place needs:"<<endl;
+	//	rep0(i,tnum)
+		//cout<<dst[fnum][i]<<" ";
+	cout<<search()<<endl;
+		}
+	return 0;
 }
